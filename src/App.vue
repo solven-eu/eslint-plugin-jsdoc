6125b2f78543de3bbf871bc13a729f5a935eabf5
David-Charlesia <pro.charlesia.david@outlook.com>
2021-09-24 14:29:49 +0200

    add a simple vue app and add the function into App.vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="myApplication">
+    <h1>Hello World</h1>
+    <p>little text behind the title</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  methods: {
+    /**
+     * @description jsLint throws errors
+     * @see https://someWebSite.com/
+     * @param {int} param - The number to be added
+     * @returns {int} 2 + param
+     */
+    // eslint-disable-next-line no-unused-vars
+    twoplusparam(param) {
+      return 2 + param;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/test.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import App from './App';
+
+// eslint-disable-next-line no-new
+new Vue({
+  components: {App},
+  el: '#myApplication',
+  template: '<App/>',
+});
